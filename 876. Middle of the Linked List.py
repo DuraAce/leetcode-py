0a12e36788315a6e34cfac98d86076c483a96be2
Evangelion01 <34011564+DuraAce@users.noreply.github.com>
2025-02-24 12:39:14 -0700

    Create 876. Middle of the Linked List.py

---
+++ 876. Middle of the Linked List.py
@@ -0,0 +1,16 @@
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        n = 0 
+        cur = head
+        while cur:
+            n += 1
+            cur = cur.next
+
+        mid = n // 2 
+        # 0 1 2 3 n = 4, mid = 2
+        # 0 1 2 3 4 n = 5, mid = 2
+        res = head
+        for _ in range(mid):
+            res = res.next
+
+        return res
