69848b80fbfa2145c26dabd7119a97ee7d497d08
Evangelion01 <34011564+DuraAce@users.noreply.github.com>
2025-02-26 10:19:49 -0700

    DP! Create 198. House Robber.py

---
+++ 198. House Robber.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * (n + 2)
+        for i, x in enumerate(nums):
+            dp[i+2] = max(dp[i+1], dp[i] + nums[i])
+        return dp[n+1]
