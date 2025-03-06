c50fe68d61dc5903a4d96fb6487e0610b2abc541
Evangelion01 <34011564+DuraAce@users.noreply.github.com>
2025-03-06 15:21:16 -0700

    dp and dfs with cache

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -5,3 +5,18 @@ class Solution:
         for i, x in enumerate(nums):
             dp[i+2] = max(dp[i+1], dp[i] + nums[i])
         return dp[n+1]
+
+
+
+# DFS solution
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def dfs(i):
+            if i < 0:
+                return 0
+            res = max(dfs(i - 1), dfs(i - 2) + nums[i])
+            return res
+        n = len(nums)
+        ans = dfs(n - 1)
+        return ans
